a05b8a5a31fc7182b828f7773b931580626f3954
practicaskary2 <practicasgithub2@gmail.com>
2021-07-27 17:15:34 -0500

    Obtener los datos de una API, convertir la respuesta

--- src/main/java/com/mycompany/gatitos_app/GatoService.java
+++ src/main/java/com/mycompany/gatitos_app/GatoService.java
@@ -5,10 +5,54 @@
  */
 package com.mycompany.gatitos_app;
 
+import com.google.gson.Gson;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author Karina
  */
 public class GatoService {
     
+    public static void verGatitos() throws IOException{
+        //1. Traer los datos de la API
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url("https://api.thecatapi.com/v1/images/search").get().build();
+        Response response = client.newCall(request).execute();
+        
+        String elJson = response.body().string();
+        
+        //Cortar los corchetes
+        elJson = elJson.substring(1,elJson.length());
+        elJson = elJson.substring(0,elJson.length()-1);
+        
+        //Crear un objeto de la clase Gson
+        Gson gson = new Gson();
+        Gato gatos = gson.fromJson(elJson, Gato.class);
+        
+        //Redimensionar la imagen
+        Image image = null;
+        try {
+            URL url = new URL(gatos.getUrl());
+            image = ImageIO.read(url);
+            
+            ImageIcon fondoGato = new ImageIcon(image);
+            
+            if(fondoGato.getIconWidth() > 800){
+                Image fondo = fondoGato.getImage();
+                Image modificada = fondo.getScaledInstance(800,600, java.awt.Image.SCALE_SMOOTH);
+                fondoGato = new ImageIcon(modificada);
+            }
+            
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
 }

8d09564d6ceb1150647b860208bc8d4be22fcd83
practicaskary2 <practicasgithub2@gmail.com>
2021-07-27 14:12:29 -0500

    Crear proyecto gatitos_app, agregar dependencias, crear clase inicio y gato

---
+++ src/main/java/com/mycompany/gatitos_app/Gato.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gatitos_app;
+
+/**
+ *
+ * @author Karina
+ */
+public class Gato {
+    
+    int id;
+    String url;
+    String apikey = "882a7e0f-bfb5-49b7-aff4-d57075054a0f";
+    String image;
+    
+}
---
+++ src/main/java/com/mycompany/gatitos_app/Inicio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gatitos_app;
+
+/**
+ *
+ * @author Karina
+ */
+public class Inicio {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}

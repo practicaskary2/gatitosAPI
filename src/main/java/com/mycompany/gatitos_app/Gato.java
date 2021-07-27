5b2b831ef4a2110b71d50b1fa7c4f31f3b1fb308
practicaskary2 <practicasgithub2@gmail.com>
2021-07-27 16:56:18 -0500

    Crear menu de opciones en la clase inicio, crear clase GatoService

--- src/main/java/com/mycompany/gatitos_app/Gato.java
+++ src/main/java/com/mycompany/gatitos_app/Gato.java
@@ -15,5 +15,39 @@ public class Gato {
     String url;
     String apikey = "882a7e0f-bfb5-49b7-aff4-d57075054a0f";
     String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
     
 }
--- src/main/java/com/mycompany/gatitos_app/Inicio.java
+++ src/main/java/com/mycompany/gatitos_app/Inicio.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.gatitos_app;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Karina
@@ -12,6 +14,31 @@ package com.mycompany.gatitos_app;
 public class Inicio {
     
     public static void main(String[] args) {
+        
+        int opcion_menu = -1;
+        String[] botones = {"1. Ver gatos", "2. Salir"};
+        
+        do{
+           //Menú Principal 
+            String opcion = (String) JOptionPane.showInputDialog(null,"Gatitos Java", "Menu Principal", JOptionPane.INFORMATION_MESSAGE,
+                    null,botones,botones[0]);
+            
+            //Validar que opción selecciona el usuario
+            for(int i=0;i<botones.length;i++){
+                if(opcion.equals(botones[i])){
+                    opcion_menu = 1;
+                }
+            }
+            
+            switch(opcion_menu){
+                case 0:
+                    GatoService.verGatitos();
+                    break;
+                default:
+                    break;
+            }
+            
+        }while(opcion_menu != 1);
         
     }
     
